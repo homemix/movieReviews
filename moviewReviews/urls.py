893660616945754d2db2765900fbc1ac267838d6
homemix <kenmutati@gmail.com>
2023-12-11 10:21:03 +0300

    Managing django urls

--- moviewReviews/urls.py
+++ moviewReviews/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from movie import views as movie_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", movie_views.home),
+    path("about/", movie_views.about),
 ]
